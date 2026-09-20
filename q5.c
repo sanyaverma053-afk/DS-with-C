fa5a14b200b9de39be4e76b8340bc3dd25f06eed
SANYA VERMA <sanyaverma053@gmail.com>
2026-09-21 00:39:22 +0530

    Implement linked list with create, display, and insert functions
    This program implements a simple linked list in C with options to create, display, and insert nodes at the beginning.

---
+++ q5.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int info;
+    struct node *next;
+};
+struct node *create(struct node *head);
+void display(struct node *head);
+struct node *insertAtBeg(struct node *head);
+int main()
+{
+    struct node *head=NULL;
+    int choice;
+    while(1)
+    {
+        printf("Press 1:create 2:display 3:insertAtBeg 0:exit\n");
+        printf("\nenter choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:head=create(head);break;
+            case 2:display(head);break;
+            case 3:head=insertAtBeg(head);break;
+            case 0:exit(1);break;
+            default:printf("wrong choice ...");
+        }
+    }
+    return 0;
+}
+struct node *create(struct node *head)
+{
+    struct node *temp, *ptr;
+    int data;
+    temp=(struct node *)malloc(sizeof(struct node));
+    printf("enter info:");
+    scanf("%d",&data);
+    temp->info=data;
+    temp->next=NULL;
+    if(head==NULL)
+    {
+        head=temp;
+    }
+    else
+    {
+        ptr=head;
+        while(ptr->next!=NULL)
+        {
+            ptr=ptr->next;
+        }
+        ptr->next=temp;
+    }
+    return head;
+}
+void display(struct node *head)
+{
+    struct node *ptr=head;
+    if(head==NULL)
+        printf("empty linked list...\n");
+    else
+    {
+        printf("\n linked list:\n");
+        while(ptr!=NULL)
+        {
+            printf("%d->",ptr->info);
+            ptr=ptr->next;
+        }
+        printf("NULL\n");
+    } 
+}
+struct node *insertAtBeg(struct node *head)
+{
+    struct node *temp,*ptr;
+    int data;
+    temp=(struct node *)malloc(sizeof(struct node ));
+    printf("enter info at beginning of linked list:");
+    scanf("%d",&data);
+    temp->info=data;
+    temp->next=NULL;
+    ptr=head;
+    temp->next=ptr;
+    head=temp;
+    printf("node inserted successfully:");
+    return head;
+}
